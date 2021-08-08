04430ceeff6ab893aea9eb3b71e599b396ddb221
lighthouse-dev <lighthouse2.dev@gmail.com>
2021-08-08 13:43:43 +0900

    ✨　FEAT: upload sample workflow

---
+++ index.js
@@ -0,0 +1,8 @@
+import alfy from 'alfy';
+
+const items = [
+  { title: 'hello, Alfred', subtitle: 'alfred subtitle', arg: 'alfred' },
+  { title: 'hello, Node', subtitle: 'node subtitle', arg: 'node' }
+];
+
+alfy.output(items);
